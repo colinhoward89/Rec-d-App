c2c1ff7e89f98154312ec00f2f42a5befee06136
Colin Howard <colinhoward89@gmail.com>
2023-05-09 20:16:01 +0100

    Simplified imports

--- client/src/Components/friend-list/friend-list.js
+++ client/src/Components/friend-list/friend-list.js
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './friend-list.module.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import EditIcon from '@mui/icons-material/Edit';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import userService from './../../Services/UserService';
