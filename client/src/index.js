5527ad80befa1c1500f907c1d52ef5e1bc55960a
Colin Howard <colinhoward89@gmail.com>
2023-05-11 16:42:35 +0100

    Setting up Auth0

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+      domain="rec-d.uk.auth0.com"
+      clientId="TS1WV9zdKs1idZTC0dqjQ0k4wvsU24Gu"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
     <App />
+    </Auth0Provider>,
   </React.StrictMode>
 );
 
--- server/controllers/videogame-search.js
+++ server/controllers/videogame-search.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-require('dotenv').config();
 
 async function getSearchResults(query) {
   try {
