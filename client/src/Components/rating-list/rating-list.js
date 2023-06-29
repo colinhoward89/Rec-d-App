c05a989810cbe13de47e8fd3e42854015156fd9d
Colin Howard <colinhoward89@gmail.com>
2023-06-29 12:53:47 +0100

    Updated styling and responsive elements

--- client/src/Components/rating-list/rating-list.js
+++ client/src/Components/rating-list/rating-list.js
@@ -27,6 +27,10 @@ const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
   textAlign: 'center',
   color: 'black',
+  fontWeight: 'lighter',
+  fontSize: '14px',
+  padding: '5px',
+  margin: '3px',
 }));
 
 const StyledRating = styled(Rating)({
@@ -83,8 +87,6 @@ function RatingList() {
       });
   }
 
-  console.log(recs)
-
   function toggleRatingPop(rec) {
     setSelectedRec(rec);
     setRatingSeen(!ratingSeen);
@@ -113,39 +115,39 @@ function RatingList() {
               {recs.map((rec) => (
                 <TableRow
                   key={rec._id}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 }, '& td': { padding: '6px 16px' } }}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 }, '& td': { padding: '0px 0px' } }}
                 >
                   <TableCell component="th" scope="row" sx={{ padding: '10px' }}>
                     {rec.type === 'book' ? (
                       rec.image ? (
                         <div style={{ paddingLeft: '10px' }}>
-                        <img src={rec.image} alt={rec.title} style={{ width: '100px', height: '100px' }} />
+                          <img src={rec.image} alt={rec.title} style={{ width: '100px', height: '100px' }} />
                         </div>
                       ) : (
                         <div style={{ paddingLeft: '10px' }}>
-                        <img src="https://st.depositphotos.com/1815808/1437/i/600/depositphotos_14371949-stock-photo-old-books-background.jpg" alt={rec.title} style={{ width: '100px', height: '100px' }} />
-                      </div>
+                          <img src="https://st.depositphotos.com/1815808/1437/i/600/depositphotos_14371949-stock-photo-old-books-background.jpg" alt={rec.title} style={{ width: '100px', height: '100px' }} />
+                        </div>
                       )
                     ) : rec.type === 'video' ? (
                       <div style={{ paddingLeft: '10px' }}>
-                      <img src="https://cdn.pixabay.com/photo/2016/06/29/14/17/joystick-1486908_640.png" alt={rec.title} style={{ width: '100px', height: 'auto' }} />
+                        <img src="https://cdn.pixabay.com/photo/2016/06/29/14/17/joystick-1486908_640.png" alt={rec.title} style={{ width: '100px', height: 'auto' }} />
                       </div>) : rec.type === 'board' ? (
+                        <div style={{ paddingLeft: '10px' }}>
+                          <img src="https://www.ageukmobility.co.uk/media/cache/default_530/upload/62/48/624887001a83bfa9e086aab9090cff8c8b51f234.jpeg" alt={rec.title} style={{ width: 'auto', height: '100px' }} />
+                        </div>) : rec.type === 'movie' || rec.type === 'tv' ? (
+                          <div style={{ paddingLeft: '10px' }}>
+                            <img src={rec.image} alt={rec.title} style={{ width: 'auto', height: '100px' }} />
+                          </div>) : (
                       <div style={{ paddingLeft: '10px' }}>
-                      <img src="https://www.ageukmobility.co.uk/media/cache/default_530/upload/62/48/624887001a83bfa9e086aab9090cff8c8b51f234.jpeg" alt={rec.title} style={{ width: 'auto', height: '100px' }} />
-                      </div>) : rec.type === 'movie' || rec.type === 'tv' ? (
-                      <div style={{ paddingLeft: '10px' }}>
-                      <img src={rec.image} alt={rec.title} style={{ width: 'auto', height: '100px' }} />
-                      </div>) : (
-                      <div style={{ paddingLeft: '10px' }}>
-                      <img src={rec.image} alt={rec.title} style={{ width: 'auto', height: '100px' }} />
+                        <img src={rec.image} alt={rec.title} style={{ width: 'auto', height: '100px' }} />
                       </div>)}
                   </TableCell>
                   <TableCell sx={{ padding: '0px' }}>
                     {isSmallScreen ? (
                       <>
                         <Stack direction="column" spacing={0} justifyContent="left" alignItems="flex-start">
-                          <Item>{rec.title}</Item>
-                          <Item>{rec.author}</Item>
+                          <Item><span style={{ fontStyle: 'italic', fontWeight: 'bolder' }}>{rec.title}</span> ({rec.year})</Item>
+                          <Item><span style={{ fontWeight: 'bold' }}>{rec.author}</span></Item>
                         </Stack>
                         {rec.sources.map((source, index) => (
                           <Stack direction="row" key={index}>
@@ -169,75 +171,77 @@ function RatingList() {
                         ))}
                       </>
                     ) : (
-                        <>
-                          <Stack direction="row" spacing={0} justifyContent="left" alignItems="flex-start">
-                            <Item>
-                              {rec.type === 'music' && <MusicNoteIcon fontSize="" />}
-                              {rec.type === 'movie' && <MovieIcon fontSize="" />}
-                              {rec.type === 'tv' && <TvIcon fontSize="" />}
-                              {rec.type === 'book' && <BookIcon fontSize="" />}
-                              {rec.type === 'video' && <VideogameAssetIcon fontSize="" />}
-                              {rec.type === 'board' && <CasinoIcon fontSize="" />}
-                            </Item>
-                            <Item>{rec.title}</Item>
-                            <Item>{rec.author}</Item>
-                            <Item>{rec.year}</Item>
-                          </Stack>
-                          <Stack direction="column">
-                            {rec.sources.map((source, index) => (
-                              <Stack direction="row" key={index}>
-                                {source.source && (
-                                  <Item>
-                                    {options.find((option) => option.id === source.source)?.name.charAt(0).toUpperCase() +
-                                      options.find((option) => option.id === source.source)?.name.slice(1)}
-                                  </Item>
-                                )}
-                                <Item>{new Date(rec.ratingDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</Item>
-                                {source.sourceComment && (
-                                  <Item style={{ display: 'flex', alignItems: 'center', height: '20px' }}>
-                                    <p>
-                                      <span className="tooltip">
-                                        <ChatBubbleOutlineIcon />
-                                        <span className="tooltiptext">{source.sourceComment}</span>
-                                      </span>
-                                    </p>
-                                  </Item>
-                                )}
-                              </Stack>
-                            ))}
-                          </Stack>
-                        </>
-                      )}
+                      <>
+                        <Stack direction="row" spacing={0} justifyContent="left" alignItems="flex-start">
+                          <Item>
+                            {rec.type === 'music' && <MusicNoteIcon fontSize="" />}
+                            {rec.type === 'movie' && <MovieIcon fontSize="" />}
+                            {rec.type === 'tv' && <TvIcon fontSize="" />}
+                            {rec.type === 'book' && <BookIcon fontSize="" />}
+                            {rec.type === 'video' && <VideogameAssetIcon fontSize="" />}
+                            {rec.type === 'board' && <CasinoIcon fontSize="" />}
+                          </Item>
+                          <Item><span style={{ fontWeight: 'bold', fontStyle: 'italic' }}>{rec.title}</span> ({rec.year})</Item>
+                          <Item>{rec.author}</Item>
+                        </Stack>
+                        <Stack direction="column">
+                          {rec.sources.map((source, index) => (
+                            <Stack direction="row" key={index}>
+                              {source.source && (
+                                <Item>
+                                  {options.find((option) => option.id === source.source)?.name.charAt(0).toUpperCase() +
+                                    options.find((option) => option.id === source.source)?.name.slice(1)}
+                                </Item>
+                              )}
+                              <Item>{new Date(rec.ratingDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</Item>
+                              {source.sourceComment && (
+                                <Item style={{ display: 'flex', alignItems: 'center', height: '20px' }}>
+                                  <p>
+                                    <span className="tooltip">
+                                      <ChatBubbleOutlineIcon />
+                                      <span className="tooltiptext">{source.sourceComment}</span>
+                                    </span>
+                                  </p>
+                                </Item>
+                              )}
+                            </Stack>
+                          ))}
+                        </Stack>
+                      </>
+                    )}
                   </TableCell>
                   <TableCell sx={{ padding: '0px' }}>
-                    <Stack direction="row" alignItems="center">
-                      {isSmallScreen && typeof rec.rating === 'number' ? (
-                        <p>{rec.rating} <StarIcon style={{ verticalAlign: 'middle', color: "orange" }} /></p>
-                      ) : (
-                        typeof rec.rating === 'number' && (
-                          <Rating
-                            value={rec.rating}
-                            precision={0.5}
-                            emptyIcon={<StarIcon fontSize="inherit" />}
-                            readOnly
-                          />
-                        )
-                      )}
-                      {rec.ratingComment && (
-                        <p>
-                          <span className="tooltip">
-                            <ChatBubbleOutlineIcon style={{ verticalAlign: 'top', height: '20px' }} />
-                            <span className="tooltiptext">{rec.ratingComment}</span>
-                          </span>
-                        </p>
-                      )}
+                    <Stack direction="column" alignItems="flex-end" sx={{ paddingRight: '20px' }}>
+                      <p>
+                        <Button variant="contained" size='small' style={{ width: isSmallScreen ? smallButtonWidth : buttonWidth }} onClick={() => toggleSendRecPop(rec)}>Rec{!isSmallScreen && 'ommend'}</Button>
+                      </p>
+                      <Stack direction="row" justifyContent="flex-end" alignItems="center">
+                        {rec.ratingComment && (
+                          <p>
+                            <span className="tooltip">
+                              <ChatBubbleOutlineIcon style={{ verticalAlign: 'top', height: '20px' }} />
+                              <span className="tooltiptext">{rec.ratingComment}</span>
+                            </span>
+                          </p>
+                        )}
+                        {isSmallScreen && typeof rec.rating === 'number' ? (
+                          <p style={{ display: 'flex', alignItems: 'center', margin: '0' }}>{rec.rating} <StarIcon style={{ verticalAlign: 'middle', color: "orange", marginLeft: '6px' }} /></p>
+                        ) : (
+                          typeof rec.rating === 'number' && (
+                            <Rating
+                              value={rec.rating}
+                              precision={0.5}
+                              emptyIcon={<StarIcon fontSize="inherit" />}
+                              readOnly
+                              style={{ margin: '4px 0' }}
+                            />
+                          )
+                        )}
+                      </Stack>
+                      <p>
+                        <Button variant="contained" size='small' style={{ width: isSmallScreen ? smallButtonWidth : buttonWidth }} onClick={() => toggleRatingPop(rec)} startIcon={<EditIcon />}>{!isSmallScreen && 'Edit'}</Button>
+                      </p>
                     </Stack>
-                    <p>
-                      <Button variant="contained" size='small' style={{ width: isSmallScreen ? smallButtonWidth : buttonWidth }} onClick={() => toggleRatingPop(rec)} startIcon={<EditIcon />}>{!isSmallScreen && 'Edit'}</Button>
-                    </p>
-                    <p>
-                      <Button variant="contained" size='small' style={{ width: isSmallScreen ? smallButtonWidth : buttonWidth }} onClick={() => toggleSendRecPop(rec)}>Rec{!isSmallScreen && 'ommend'}</Button>
-                    </p>
                   </TableCell>
                 </TableRow>
               ))}
