7195f8f994920f657397132f041534f2c960312c
Colin Howard <colinhoward89@gmail.com>
2023-07-05 16:46:39 +0100

    Removing local host

--- client/src/Services/TVService.js
+++ client/src/Services/TVService.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 const tvService = {};
 
--- client/src/Services/UserService.js
+++ client/src/Services/UserService.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
 export const register = (user) => {
   return fetch(`${BASE_URL}/register`, {
